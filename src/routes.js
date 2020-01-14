7ea64aa4769629cd771b09423257fb83a196703f
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-14 13:49:47 -0300

    criado rota para cadastro de dev

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,8 @@ const { Router } = require('express');
 
 const routes = Router();
 
+const DevController = require('./controllers/DevController');
+
+routes.post('/devs', DevController.store)
+
 module.exports = routes;
