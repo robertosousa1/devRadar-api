bdba20c302912cb069b9c9426a845c3c3a51e5d2
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-19 23:54:00 -0300

    utilizando métodos socket

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async index(req, res) {
@@ -33,7 +34,14 @@ module.exports = {
         bio,
         techs: techsArray,
         location
-      })            
+      })
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      )
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev)
     }
 
     return res.json(dev);
